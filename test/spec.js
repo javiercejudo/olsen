dc9f72345ff6224d0a4fa5d92ee0574270fa8505
Javier Cejudo <javier@javiercejudo.com>
2017-04-29 09:59:07 +1000

    test(use-case): refactor to use easier binding of ptototype functions (#21)

--- test/spec.js
+++ test/spec.js
@@ -16,22 +16,22 @@ function identity(x) {
 
 describe('olsen', function() {
   it('should validate two of a kind', function() {
-    olsen(1, 1)(Number).should.be.true;
+    olsen(1, 1)(Number).should.be.exactly(true);
     olsen(1, 0.99999999999999999)(Number).should.be.true; // 0.999... becomes exactly 1
-    olsen(1, '1.0')(Number).should.be.true;
+    olsen(1, '1.0')(Number).should.be.exactly(true);
 
-    olsen(1, 3)(isEven).should.be.true; // both of kind "not even"
-    olsen(2, 4)(isEven).should.be.true; // both of kind "even"
+    olsen(1, 3)(isEven).should.be.exactly(true); // both of kind "not even"
+    olsen(2, 4)(isEven).should.be.exactly(true); // both of kind "even"
 
-    olsen(Math.PI / 4, (7 / 4) * Math.PI)(function(x) {
+    olsen(Math.PI / 4, 7 / 4 * Math.PI)(function(x) {
       return Math.cos(x).toFixed(10);
-    }).should.be.true;
+    }).should.be.exactly(true);
   });
 
   it('should invalidate different kinds', function() {
-    olsen(1, 2)(isEven).should.be.false;
-    olsen(1, 0.9999999999999999)(Number).should.be.false;
-    olsen(1, '1.0')(JSON.stringify).should.be.false;
-    olsen(NaN, NaN)(identity).should.be.false; // NaN !== NaN
+    olsen(1, 2)(isEven).should.be.exactly(false);
+    olsen(1, 0.9999999999999999)(Number).should.be.exactly(false);
+    olsen(1, '1.0')(JSON.stringify).should.be.exactly(false);
+    olsen(NaN, NaN)(identity).should.be.exactly(false); // NaN !== NaN
   });
 });
